b3ba8a0582b61faeb231fe8e9531f4168ade841f
Stephen Feng <fengstep@pdx.edu>
2025-08-04 19:06:59 -0700

    REFACTOR test_searcher

--- ftp_client/tests/test_searcher.py
+++ ftp_client/tests/test_searcher.py
@@ -8,39 +8,29 @@ from ..searcher import Searcher
 from ..ftp_client_exceptions import ClientNoPathProvidedError
 
 class TestSearcher(unittest.IsolatedAsyncioTestCase):
-    async def connect(self):
+    async def asyncSetUp(self):
         load_dotenv("public.env")
-        aio_client = aioftp.Client()
-        await aio_client.connect(os.getenv("ip"), os.getenv("port"))
-        await aio_client.login("user", "password")
-        return aio_client
+        self.client = aioftp.Client()
+        await self.client.connect(os.getenv("ip"), os.getenv("port"))
+        await self.client.login("user", "password")
+        self.searcher = Searcher(self.client)
+    async def asyncTearDown(self):
+        await self.client.quit()
     async def test_search_not_found(self):
-        client = await self.connect()
-        try_search = Searcher(client)
-        result = await try_search.search("KADJSFLKJSDFLJ.exe")
+        result = await self.searcher.search("KADJSFLKJSDFLJ.exe")
         assert result == False
     async def test_search_error(self):
-        client = await self.connect()
-        try_search = Searcher(client)
         with pytest.raises(ClientNoPathProvidedError):
-            result = await try_search.search(" ")
+            result = await self.searcher.search(" ")
     async def test_search_found(self):
-        client = await self.connect()
-        try_search = Searcher(client)
-        result = await try_search.search("this_is_remote.txt")
+        result = await self.searcher.search("this_is_remote.txt")
         assert result == True
     async def test_local_search_error(self):
-        client = await self.connect()
-        try_search = Searcher(client)
         with pytest.raises(ClientNoPathProvidedError):
-            result = try_search.local_search("")
+            result = self.searcher.local_search("")
     async def test_local_search_found(self):
-        client = await self.connect()
-        try_search = Searcher(client)
-        result = try_search.local_search("this_is_local.txt")
+        result = self.searcher.local_search("this_is_local.txt")
         assert result == True
     async def test_local_search_not_found(self):
-        client = await self.connect()
-        try_search = Searcher(client)
-        result = try_search.local_search("asdfjaslkfjaslkdfjalsf.txt")
+        result = self.searcher.local_search("asdfjaslkfjaslkdfjalsf.txt")
         assert result == False
